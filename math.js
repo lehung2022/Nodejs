21a8d65670eeee564c4d1eac4d367960a69e7834
lehung2022 <hung.le.programmer@gmail.com>
2023-10-05 09:43:59 +0700

    first chapter was done

---
+++ math.js
@@ -0,0 +1,14 @@
+exports.add = (a, b) => {
+    return a + b;
+}
+exports.multiply = (c, d) => {
+    return c * d;
+}
+exports.subtract = (e, f) => {
+    return e - f;
+}
+exports.divide = (g, h) => {
+    return g / h;
+}
+
+// module.exports = { add, multiply, subtract, divide }
---
+++ server.js
@@ -0,0 +1,7 @@
+
+const {add, subtract, multiply, divide} = require("./math");
+console.log(add(2, 3));
+
+
+// const math = require("./math")
+// console.log(math.multiply(2, 3))
